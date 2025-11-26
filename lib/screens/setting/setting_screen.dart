98300909e323f5c88e218ac6344a9412c8c3e0e6
Nishan <girinishan1234567890@gmail.com>
2025-11-26 17:12:37 +0545

    created setting screens with some widgets

--- lib/screens/setting/setting_screen.dart
+++ lib/screens/setting/setting_screen.dart
@@ -1,10 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:yumm_ai/screens/setting/widgets/profile_preview_card.dart';
+import 'package:yumm_ai/screens/setting/widgets/setting_item_card.dart';
+import 'package:yumm_ai/screens/setting/widgets/setting_list_group.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Placeholder()));
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings")),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 18),
+            child: Column(
+              spacing: 12,
+              children: [
+                ProfilePreviewCard(),
+                SizedBox(height: 12),
+                SettingListGroup(
+                  groupName: "Account",
+                  settingLists: [
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.user,
+                      title: 'Account access',
+                      subTitle:
+                          'Security, account activation and deactivation.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.shield,
+                      title: 'Login & Security',
+                      subTitle: 'Manage your information on this app.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.dollar_sign,
+                      title: 'Manage Subscription',
+                      subTitle:
+                          'Change your subscription plan for more access.',
+                    ),
+                  ],
+                ),
+                Divider(),
+                SettingListGroup(
+                  groupName: "Preferences",
+                  settingLists: [
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.bell,
+                      title: 'Notifications',
+                      subTitle: 'Select the kind of notification you get.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.sliders_horizontal,
+                      title: ' Food Preferences',
+                      subTitle:
+                          'Tune your likings and food habit on daily basis.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.book,
+                      title: 'Accessibility & languages',
+                      subTitle: 'Manage how contents are displayed to you.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.sun,
+                      title: 'App theme',
+                      subTitle:
+                          'Change the theme of the app as per your likings.',
+                    ),
+                  ],
+                ),
+                Divider(),
+                SettingListGroup(
+                  groupName: "Additional",
+                  settingLists: [
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.smile,
+                      title: 'Feedback',
+                      subTitle: 'Share your thoughts and help us improve',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.box,
+                      title: 'Additional resources',
+                      subTitle: 'Checkout other places to know more about app.',
+                    ),
+                    SettingItemCard(
+                      leadingIcon: LucideIcons.info,
+                      title: 'Help',
+                      subTitle: 'Help center, contacts, privacy policy.',
+                    ),
+                  ],
+                ),
+                SizedBox(height: 108),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/setting/widgets/profile_preview_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:yumm_ai/core/styles/app_colors.dart';
+import 'package:yumm_ai/core/styles/app_text_styles.dart';
+
+class ProfilePreviewCard extends StatelessWidget {
+  const ProfilePreviewCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        spacing: 8,
+        children: [
+          CircleAvatar(radius: 30, backgroundColor: AppColors.blackColor),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Username",
+                style: AppTextStyles.title.copyWith(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                "email@example.com",
+                style: AppTextStyles.normalText.copyWith(
+                  color: AppColors.descriptionTextColor,
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          Icon(LucideIcons.chevron_right, size: 30),
+        ],
+      ),
+    );
+  }
+}
